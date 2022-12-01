db7c1e64db032cceb05b020e472eb98339e13763
Denis Bezukladnikov <denis3094@mail.ru>
2022-12-01 21:31:44 +0700

    feat: подключить normalize, шрифты, организовать структуру проекта

---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="page">
+      <div>Header</div>
+      <div>Content</div>
+      <div>Footer</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            Войти
+        </div>
+    );
+};
+
+export default Login;
