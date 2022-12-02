53999f32af6468c673f96d42897bc9531b425233
Denis Bezukladnikov <denis3094@mail.ru>
2022-12-02 23:54:11 +0700

    feat: добавить компонент header

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import "./App.scss";
+import Header from "../Header/Header";
 
 function App() {
-  return (
-    <div className="page">
-      <div>Header</div>
-      <div>Content</div>
-      <div>Footer</div>
-    </div>
-  );
+    return (
+        <div className="page">
+            <Header/>
+            <div>Content</div>
+            <div>Footer</div>
+        </div>
+    );
 }
 
 export default App;
