5bf17c56ebd4279a9f5af030d9cb9e0ce8fa3b66
Denis Bezukladnikov <denis3094@mail.ru>
2022-12-04 20:44:41 +0700

    feat: организовать роутинг, обернуть контент в компонент main

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,21 +6,22 @@ import {Route, Routes, useLocation} from "react-router-dom";
 import TodoList from "../TodoList/TodoList";
 import Login from "../Login/Login";
 import Register from "../Register/Register";
+import Main from "../Main/Main";
 
 function App() {
     const location = useLocation()
 
     return (
         <div className="page">
-
-            <Routes>
-                <Header/>
-                <Route path="/" element={<TodoList/>}></Route>
-                <Route path="/signin" element={<Login/>}></Route>
-                <Route path="/signup" element={<Register/>}></Route>
-                <Footer/>
-            </Routes>
-
+            <Header/>
+            <Main>
+                <Routes>
+                    <Route path="/" element={<TodoList/>}></Route>
+                    <Route path="/signin" element={<Login/>}></Route>
+                    <Route path="/signup" element={<Register/>}></Route>
+                </Routes>
+            </Main>
+            <Footer/>
         </div>
     );
 }
