29d1afe5358ffe3990975e71826b5a460b96665f
Denis Bezukladnikov <denis3094@mail.ru>
2022-12-04 20:37:40 +0700

    feat: организовать роутинг

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,13 +2,25 @@ import React from "react";
 import "./App.scss";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import {Route, Routes, useLocation} from "react-router-dom";
+import TodoList from "../TodoList/TodoList";
+import Login from "../Login/Login";
+import Register from "../Register/Register";
 
 function App() {
+    const location = useLocation()
+
     return (
         <div className="page">
-            <Header/>
-            <div>Content</div>
-            <Footer/>
+
+            <Routes>
+                <Header/>
+                <Route path="/" element={<TodoList/>}></Route>
+                <Route path="/signin" element={<Login/>}></Route>
+                <Route path="/signup" element={<Register/>}></Route>
+                <Footer/>
+            </Routes>
+
         </div>
     );
 }
