2ef74c4003f01032148c0ee93374f9fa011c411d
Denis Bezukladnikov <denis3094@mail.ru>
2022-12-05 17:48:15 +0700

    feat: добавить инпуты в компонент register

--- src/components/Main/Auth/Register/Register.tsx
+++ src/components/Main/Auth/Register/Register.tsx
@@ -1,18 +1,43 @@
-import React from 'react';
+import React from "react";
 import Auth from "../Auth";
+import Input from "../Input/Input";
 
 const Register = () => {
-    return (
-        <Auth
-            title="Добро пожаловать!"
-            name="register"
-            buttonName="Зарегистрироваться"
-            question="Уже зарегистрированы?"
-            pathName="Войти"
-            path="/signin"
-        >
-        </Auth>
-    );
+  return (
+    <Auth
+      title="Добро пожаловать!"
+      name="register"
+      buttonName="Зарегистрироваться"
+      question="Уже зарегистрированы?"
+      pathName="Войти"
+      path="/signin"
+    >
+      <Input
+        labelText="Имя"
+        type="text"
+        name="username"
+        minLength={2}
+        maxLength={30}
+        placeholder="Как Вас зовут?"
+      />
+      <Input
+        labelText="E-mail"
+        type="email"
+        name="email"
+        minLength={4}
+        maxLength={16}
+        placeholder="Введите e-mail"
+      />
+      <Input
+        labelText="Пароль"
+        type="password"
+        name="password"
+        minLength={4}
+        maxLength={10}
+        placeholder="Введите пароль"
+      />
+    </Auth>
+  );
 };
 
 export default Register;
