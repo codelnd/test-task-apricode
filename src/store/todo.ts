081bd3190a637381b031f66f8621ba995f6dd641
Denis Bezukladnikov <denis3094@mail.ru>
2022-12-07 16:57:44 +0700

    feat: описать actions

--- src/store/todo.ts
+++ src/store/todo.ts
@@ -6,6 +6,21 @@ class TodoStore {
   constructor() {
     makeAutoObservable(this);
   }
+
+  addTodo = (todo: any) => {
+    // @ts-ignore
+    this.todos.push(todo);
+  };
+
+  removeTodo = (id: any) => {
+    this.todos.filter((todo: any) => todo.id !== id);
+  };
+
+  completeTodo = (id: any) => {
+    this.todos.map((todo: any) =>
+      todo.id === id ? { complete: !todo.complete } : todo
+    );
+  };
 }
 
 export const todoStore = new TodoStore();
