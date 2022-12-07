c56d4f19f1bc0213444e59b4697d15dfa594bd3c
Denis Bezukladnikov <denis3094@mail.ru>
2022-12-08 00:59:32 +0700

    feat: Написать функцию отправки формы для добавления todo

--- src/components/Main/TodoList/TodoList.tsx
+++ src/components/Main/TodoList/TodoList.tsx
@@ -1,47 +1,59 @@
-import React, { useEffect } from "react";
-import { observer } from "mobx-react-lite";
-import { todoStore } from "../../../store/todo";
+import React, {useEffect} from "react";
+import {observer} from "mobx-react-lite";
+import {todoStore} from "../../../store/todo";
 import "./TodoList.scss";
 
 const TodoList = observer(() => {
-  useEffect(() => {
-    todoStore.fetchTodos();
-  }, []);
+    useEffect(() => {
+        todoStore.fetchTodos();
+    }, []);
 
-  console.log(todoStore.inputValue);
 
-  return (
-    <section className="todo-list">
-      <input
-        type="text"
-        value={todoStore.inputValue}
-        onChange={(e) => todoStore.addTodoValue(e.target.value)}
-      />
-      <button>Добавить</button>
-      <ul className="todos">
-        {todoStore.todos.map((todo: any) => (
-          <li className="todo" key={todo.id}>
-            <label className="todo__label">
-              {todo.title}
-              <input
-                className="todo__status"
-                type="checkbox"
-                checked={todo.completed}
-                onChange={() => todoStore.completeTodo(todo.id)}
-              />
-            </label>
-            <button
-              className="todo__remove"
-              onClick={() => todoStore.removeTodo(todo.id)}
-              type="button"
-            >
-              Удалить
-            </button>
-          </li>
-        ))}
-      </ul>
-    </section>
-  );
+    const handleAddTodo = (e: React.FormEvent) => {
+        e.preventDefault()
+        if (!todoStore.inputValue) {
+            return
+        } else {
+            console.log(todoStore.inputValue);
+        }
+        todoStore.inputValue = ""
+    }
+
+    return (
+        <section className="todo-list">
+            <form onSubmit={handleAddTodo}>
+                <input
+                    type="text"
+                    placeholder="Добавьте задачу"
+                    value={todoStore.inputValue}
+                    onChange={(e) => todoStore.addTodoValue(e.target.value)}
+                />
+                <button>Добавить</button>
+            </form>
+            <ul className="todos">
+                {todoStore.todos.map((todo: any) => (
+                    <li className="todo" key={todo.id}>
+                        <label className="todo__label">
+                            {todo.title}
+                            <input
+                                className="todo__status"
+                                type="checkbox"
+                                checked={todo.completed}
+                                onChange={() => todoStore.completeTodo(todo.id)}
+                            />
+                        </label>
+                        <button
+                            className="todo__remove"
+                            onClick={() => todoStore.removeTodo(todo.id)}
+                            type="button"
+                        >
+                            Удалить
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </section>
+    );
 });
 
 export default TodoList;
